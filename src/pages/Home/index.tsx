3b3c8855e0d1a429962172be9ed069cd3b1db352
leoweihermann <leonardoweihermann@gmail.com>
2021-09-22 20:46:58 -0300

    feat(starred-repositories): home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { Container } from './styles';
+import { useHistory } from 'react-router-dom';
+
+import Header from '../../Components/Header';
+
+import { Container, Form, Search, Title } from './styles';
 
 const Home: React.FC = () => {
+  const [inputError, setInputError] = useState('');
+  const [userName, setUsername] = useState('');
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(() => {
+    history.push(`/user/${userName}`);
+  }, [history, userName]);
+
   return (
     <Container>
-      <h1>GitHub Stars</h1>
+      <Header />
+      <Search>
+        <Title>Veja os repositórios estrelados por usuário.</Title>
+        <Form hasError={!!inputError} onSubmit={handleSubmit}>
+          <input
+            value={userName}
+            placeholder="Digite o nome do usuário"
+            onChange={e => setUsername(e.target.value)}
+          />
+          <button type="submit">Pesquisar</button>
+        </Form>
+      </Search>
     </Container>
   );
 };
