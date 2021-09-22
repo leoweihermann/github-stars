ec9052051f7465bc64b856d528fe86a9b07d5a26
leoweihermann <leonardoweihermann@gmail.com>
2021-09-22 20:44:13 -0300

    feat(starred-repositories): loading coponent

---
+++ src/Components/UserResults/Loading/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+
+  margin-left: 3rem;
+  gap: 1rem;
+`;
+
+export const LoadingUserInfo = styled.div`
+  width: 350px;
+`;
+
+export const LoadingRepositoriesList = styled.div`
+  width: 70%;
+`;
